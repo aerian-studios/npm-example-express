8b3265a3a1ac848adedf6c9eac1cb227a154fede
Ben Poole <ben.poole@aerian.com>
2018-01-08 16:46:44 +0000

    basic express app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const library = require('library');
+
+let controller = function(req, res) {
+    let libraryModule1 = library.module1;
+    res.send('Hello World - ' + libraryModule1.hello('ben'));
+};
+
+app.get('/', controller);
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+})
